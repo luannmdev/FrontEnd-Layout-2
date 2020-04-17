966a6f5523f60a1dff770ef6cfbc2c9cf5d7832c
luannm_lap <vnproben@gmail.com>
2020-04-17 17:44:28 +0700

    first commit

---
+++ myscripts.js
@@ -0,0 +1,20 @@
+//var count = 0;
+//window.onscroll = function() {
+//    myFunction();
+//}
+//function midFunction() {
+//    document.getElementById("box1").className = "mid";
+//}
+//function myFunction() {
+//    if ((document.body.scrollTop == 200) || (document.documentElement.scrollTop == 200)) {
+//        count++;
+//        alert(count);
+//        if ((count % 2) == 1) {
+//            document.getElementById("box1").className = "show";
+//        }
+//        else {
+//            document.getElementById("box1").className = "hide";
+//            setTimeout(midFunction,1000);
+//        }
+//    } 
+//}
